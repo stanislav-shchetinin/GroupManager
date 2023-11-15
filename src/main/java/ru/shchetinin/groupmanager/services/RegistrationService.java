093070ba8978308d328cedfde2ad0bb17c718cbc
Stas <stas.shc@gmail.com>
2023-11-15 10:05:37 +0300

    add logs in RegistrationService

--- src/main/java/ru/shchetinin/groupmanager/services/RegistrationService.java
+++ src/main/java/ru/shchetinin/groupmanager/services/RegistrationService.java
@@ -1,6 +1,7 @@
 package ru.shchetinin.groupmanager.services;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,7 @@ import ru.shchetinin.groupmanager.responses.Response;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class RegistrationService {
     private final UserRepository userRepo;
     private final AuthorityRepository authorityRepo;
@@ -21,13 +23,18 @@ public class RegistrationService {
 
     public Response addNewUser(User user) throws UserAlreadyExistsException {
         String login = user.getUsername();
-        if (userRepo.findByUsername(login) != null){
+        boolean isUserAlreadyExist = userRepo.findByUsername(login) != null;
+        log.debug("login: {}, isUserAlreadyExist: {}", login, isUserAlreadyExist);
+        if (isUserAlreadyExist){
             throw new UserAlreadyExistsException();
         } else {
             user.setEnabled(true);
             user.setPassword(encoder.encode(user.getPassword()));
             userRepo.save(user);
-            authorityRepo.save(new Authority(user.getUsername(), RoleAdd.ROLE_USER.name()));
+            log.debug("USER. enabled: {}, password: {}", user.isEnabled(), user.getPassword());
+            Authority authority = new Authority(user.getUsername(), RoleAdd.ROLE_USER.name());
+            authorityRepo.save(authority);
+            log.debug("AUTHORITY. username: {}, role: {}", authority.getUsername(), authority.getAuthority());
             return new Response(HttpStatus.OK.value(), "User successfully added");
         }
     }
